2328270295f229700f82e84981d6e0ca43b23f93
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 17:10:17 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'localdb' //'dedwards-cst323-azurewebapp.mysql.database.azure.com';
+$host = '127.0.0.1'; //'dedwards-cst323-azurewebapp.mysql.database.azure.com';
 $username = 'root';
 $password = '';
 $db_name = 'contactmanagement';
