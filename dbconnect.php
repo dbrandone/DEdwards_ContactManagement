319a7a7700314f6a3a7e67e026e01cb759f3f777
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 16:37:40 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,26 @@
 <?php
-$connect = new mysqli('localhost', 'root', '', 'contactmanagement');
+$host = 'dedwards-cst323-azurewebapp.mysql.database.azure.com';
+$username = 'root';
+$password = '';
+$db_name = 'contactmanagement';
 
-        if(!$connect){
-            die(mysqli_error($connect));
-        }
+//Initializes MySQLi
+$conn = mysqli_init();
+
+//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 53542, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+//$connect = new mysqli('localhost', 'root', '', 'contactmanagement');
+
+        //if(!$connect){
+            //die(mysqli_error($connect));
+        //}
     
 ?>
