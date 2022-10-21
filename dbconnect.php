d04ded8714cbe5f1e7edbbe5844f79e43a48a567
dbrandone <115263503+dbrandone@users.noreply.github.com>
2022-10-21 16:52:42 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,16 +1,19 @@
 <?php
-$host = 'dedwards-cst323-azurewebapp.mysql.database.azure.com';
+$host =  'localdb'; //'dedwards-cst323-azurewebapp.mysql.database.azure.com';
 $username = 'root';
 $password = '';
 $db_name = 'contactmanagement';
 
+$mysql_handler = mysql_connect($host, $username, $password);
+mysql_select_db($db_name, $mysql_handler);
+
 //Initializes MySQLi
-$conn = mysqli_init();
+//$conn = mysqli_init();
 
 //mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
 // Establish the connection
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 53542, NULL, MYSQLI_CLIENT_SSL);
+//mysqli_real_connect($conn, $host, $username, $password, $db_name, 53542, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
 if (mysqli_connect_errno())
